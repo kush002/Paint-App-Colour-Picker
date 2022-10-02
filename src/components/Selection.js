f2c48b4027685d6101a9cf7ec601feb018f92e65
kush002 <kushagrasingh543@gmail.com>
2022-10-03 00:36:52 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import "../styles/Child.css";
 
 const Selection = (props) => {
-  const [nextBackground, setBackground] = useState({ background: "" });
+  const [nextBackground, setBackground] = useState();
 
   const backgroundChangeHandler = () => {
     // const{nextBackground} = props
     //   setBackground(nextNewBackground)
     console.log(props.applyColor);
-    setBackground({ background: props.applyColor });
+    setBackground(props.applyColor );
   };
 
   //   const styleSelection = props.nextBackground;
@@ -18,7 +18,7 @@ const Selection = (props) => {
     <div
       className="fix-box"
       onClick={backgroundChangeHandler}
-      style={{ background: nextBackground.background }}
+      style={{ background: nextBackground}}
     >
       <h2 className="subheading">Selection</h2>
     </div>
