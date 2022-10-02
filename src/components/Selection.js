e86b8e8fe8ec317f8946cc96a637cbbd6c291d4d
kush002 <kushagrasingh543@gmail.com>
2022-10-02 20:42:34 +0530

    Add files via upload

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 import "../styles/Child.css";
 
 const Selection = (props) => {
-  const [nextNewBackground, setBackground] = useState("");
+  const [nextBackground, setBackground] = useState("");
 
   const backgroundChangeHandler = () => {
     // const{nextBackground} = props
-    setBackground(nextNewBackground)
-    props.applyColor(setBackground);
+    setBackground(props.applyColor);
+    // props.applyColor(setBackground);
   };
 
-  // const styleSelection = props.nextBackground
-  console.log(nextNewBackground);
+  //   const styleSelection = props.nextBackground;
+  //   console.log(nextNewBackground);
 
   return (
     <div
-      onClick={backgroundChangeHandler}
       className="fix-box"
-      style={{ background: nextNewBackground }}
+      onClick={backgroundChangeHandler}
+      style={{ background: nextBackground }}
     >
       <h2 className="subheading">Selection</h2>
     </div>
